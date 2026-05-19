fda67c1c211a20303f9219710da7962b2c8f7b87
Egeland, Renate <Renate.Egeland@hmhw.com>
2026-05-19 08:05:30 +0200

    ready for production

--- src/server.js
+++ src/server.js
@@ -10,7 +10,11 @@ const schemaPath = path.join(__dirname, '..', 'db', 'schema.sql');
 // ---------- App ----------
 const app = express();
 app.use(express.json());
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(require('path').join(__dirname, '..', 'public', 'index.html'));
+});
 
 function runAsync(sql, params = []) {
   return new Promise((resolve, reject) => {
